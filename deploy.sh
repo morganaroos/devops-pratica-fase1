af627ae825d2af0960cf66b30167fe34f8bf69f1
Morgana Roos <morgana.roos@gmail.com>
2026-06-22 16:00:32 -0300

    Update deployment script for EC2

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-echo "Parando containers antigos..."
-docker compose down
+APP_NAME="devops-pratica-fase2"
+IMAGE_NAME="devops-pratica-fase2"
+
+echo "Parando container antigo..."
+docker stop $APP_NAME || true
+
+echo "Removendo container antigo..."
+docker rm $APP_NAME || true
 
 echo "Criando nova imagem..."
-docker compose build
+docker build -t $IMAGE_NAME .
 
 echo "Subindo aplicação..."
-docker compose up -d
+docker run -d \
+  --name $APP_NAME \
+  -p 80:80 \
+  $IMAGE_NAME
 
 echo "Deploy concluído!"
